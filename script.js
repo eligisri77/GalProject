4531e38f53c0c11288e932e363c0950e1e18c88a
eligisri77 <65546832+eligisri77@users.noreply.github.com>
2024-08-28 17:08:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secoundBatch = 7
+
+// let count = firstBatch + secoundBatch
+
+// console.log(count)
+
+let myage = 40
+let humanDogRatio = 7
+
+let myDogAge = myage * humanDogRatio
+
+console.log(myDogAge)
